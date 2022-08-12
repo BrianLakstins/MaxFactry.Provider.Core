21ab1e5133afb5f792e0320a63334a5390f17d6a
BrianLakstins <2887390+BrianLakstins@users.noreply.github.com>
2022-08-12 14:41:53 -0400

    Add submodule

---
+++ .gitsubmodule.bat
@@ -0,0 +1,3 @@
+git submodule add https://github.com/BrianLakstins/MaxFactry.General.git _sm/MaxFactry.General
+git submodule update --init --recursive
+git submodule update --init --recursive --remote
