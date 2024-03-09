c49e84dd2834d0ceb02e9b9cf23b1e3bd89b8106
BrianLakstins <2887390+BrianLakstins@users.noreply.github.com>
2024-03-08 19:37:54 -0500

    Update references Add .NET Core 8.0 project Update NuGet packages

--- .nuget/package.bat
+++ .nuget/package.bat
@@ -5,5 +5,6 @@ copy ..\MaxFactry.Provider.CoreProvider-NF-4.7.2\bin\Release\MaxFactry.Provider.
 copy ..\MaxFactry.Provider.CoreProvider-NF-4.8\bin\Release\MaxFactry.Provider.Core*.dll lib\net48\
 copy ..\MaxFactry.Provider.CoreProvider-NC-3.1\bin\Release\netcoreapp3.1\MaxFactry.Provider.Core*.dll lib\netcoreapp3.1\
 copy ..\MaxFactry.Provider.CoreProvider-NC-6.0\bin\Release\net6.0\MaxFactry.Provider.Core*.dll lib\net6.0\
+copy ..\MaxFactry.Provider.CoreProvider-NC-8.0\bin\Release\net8.0\MaxFactry.Provider.Core*.dll lib\net8.0\
 
 c:\install\nuget\nuget.exe pack MaxFactry.Provider.Core.nuspec -OutputDirectory "packages" -IncludeReferencedProjects -properties Configuration=Release 
